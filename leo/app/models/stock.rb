342c9de18cb74eda519aceed5a136d5fada0fac9
LEO le <avril9950@gmail.com>
2019-07-04 17:52:01 +0800

    1. init 2. api: get /stock/query

---
+++ leo/app/models/stock.rb
@@ -0,0 +1,16 @@
+class Stock
+    attr_accessor :id
+    attr_accessor :company_name
+    attr_accessor :company_href
+    attr_accessor :start_price
+    attr_accessor :max_price
+    attr_accessor :min_price
+    attr_accessor :yesterday_closing_price
+    attr_accessor :today_closing_price
+    attr_accessor :volume
+    attr_accessor :up_down_value
+    attr_accessor :percentage_up_down_value
+
+    def initialize()
+    end
+end
---
+++ leo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '49a681e65e7b8b6e49f55c9885938067e3c65cc6effb3341e4239970acd204dc00f7397752ea4ac6704389b320f85e4819b2db2ba58506f230f73c49380b7e35'
