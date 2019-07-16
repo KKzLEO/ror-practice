3d39554e04b1d0fabc4da53ee5e29840fb0bf44f
LEO le <avril9950@gmail.com>
2019-07-16 18:03:38 +0800

    Add component Turnover unit test

--- leo/frontend/src/components/Turnover/Turnover.test.js
+++ leo/frontend/src/components/Turnover/Turnover.test.js
@@ -2,8 +2,24 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Turnover from './Turnover'
 import styles from './Turnover.module.scss';
+import Immutable from 'immutable';
 
-// it('should render without crashing', () => {
-//     shallow(<Turnover />)
-// });
+it('should render without crashing', () => {
+    let props = {
+        fetchStockData: jest.fn(),
+        setQueryParameters: jest.fn().mockResolvedValue(43),
+        stockData: [],
+        isLoading: false,
+        parameters: Immutable.Map()
+    }
+    const wrapper = shallow(<Turnover {...props} />)
+    expect(props.fetchStockData).toBeCalled();
+    expect(props.fetchStockData).toBeCalledTimes(1);
+    expect(wrapper.instance().sortByField()).toBe(undefined);
+});
+
+// it('check ', () => {
+    
+// })
+
 
